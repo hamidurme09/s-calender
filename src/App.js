67ff8b20c20ce47ccec418788895c07462b790bc
Hamidur <macbook@MacBooks-MacBook-Pro.local>
2020-12-05 14:27:35 +0600

    ..

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
-import Calendar from "./CalendarDmo";
+import CalendarDmo from "./CalendarDmo";
+import Calendar from 'react-calendar'
+
+import 'react-calendar/dist/Calendar.css';
 
 function App() {
+
+  const [date, setDate] = useState(new Date())
+
+  const onChange = date => {
+    setDate(date)
+  }
+
   return (
     <div>
-      <Calendar />
+      <CalendarDmo />
+      {/* <Calendar onChange={onChange} value={date} /> */}
     </div>
   );
 }
